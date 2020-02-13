53a8a525e92a86ce6b7e90f5797ed51a7a446236
q q <acuteneedle@gmail.com>
2020-02-13 14:34:44 +0200

    adding structure

---
+++ src/Controllers/GiveawayController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GiveawayFreeSteamBot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GiveawayController : ControllerBase
+    {
+        private readonly ILogger<GiveawayController> _logger;
+
+        public GiveawayController(ILogger<GiveawayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public OkResult Get()
+        {
+            return Ok();
+        }
+    }
+}
