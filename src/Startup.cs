94698bd72be1fecd185d1ca9c63c6150520d77aa
q q <acuteneedle@gmail.com>
2020-02-19 17:22:50 +0200

    csproj sln rellocation

--- src/Startup.cs
+++ src/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GiveawayFreeSteamBot.GiveawayDiscordNotifier.src.Models;
+using GiveawayFreeSteamBot.GiveawayDiscordNotifier.src.Parser;
+using GiveawayFreeSteamBot.GiveawayDiscordNotifier.src.Repositories;
+using GiveawayFreeSteamBot.GiveawayDiscordNotifier.src.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -15,9 +19,21 @@ namespace GiveawayFreeSteamBot
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json",
+                            optional: false,
+                            reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +42,18 @@ namespace GiveawayFreeSteamBot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IGiveawayService, GiveawayService>();
+            services.AddScoped<IFeedConnector, FeedConnector>();
+            services.AddScoped<IFeedProvider, FeedProvider>();
+            services.AddScoped<IFeedParser, FeedParser>();
+            services.AddScoped<IDiscordService, DiscordService>();
+            services.AddScoped<IGiveawayRepository, GiveawayRepository>();
+            services.Configure<MongoConfig>(options =>
+            {
+                options.connectionString = Configuration["mongo:connectionString"];
+                options.dbName = Configuration["mongo:dbName"];
+                options.collectionName = Configuration["mongo:collectionName"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
